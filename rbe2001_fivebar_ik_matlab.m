0f4aa3d18671d4656c7c106cdc94cbc2b230a6c6
G-Chist <42654606+G-Chist@users.noreply.github.com>
2025-02-24 15:54:47 -0500

    Add files via upload

---
+++ rbe2001_fivebar_ik_matlab.m
@@ -0,0 +1,110 @@
+% Define parameters
+OC = 40;
+AP = 50;
+PB = 50;
+OA = 60;
+BC = 60;
+
+% Define Px and Py range for animation
+Px_values = linspace(20, 20, 100); % Varying Px
+Py_values = linspace(30, 105, 100); % Varying Py
+
+% Create figure
+figure;
+hold on;
+grid on;
+axis equal;
+xlim([-100, 200]);  % Set x-axis limits
+ylim([0, 200]);    % Set y-axis limits
+xlabel('X-axis');
+ylabel('Y-axis');
+title('Five Bar Mechanism Animation');
+
+% Loop over different Px and Py values
+for i = 1:length(Px_values)
+    Px = Px_values(i);
+    Py = Py_values(i);
+    
+    % Solve for A and B positions
+    syms xA yA xB yB;
+    
+    eqOA = xA^2 + yA^2 == OA^2;
+    eqPA = (xA - Px)^2 + (yA - Py)^2 == AP^2;
+    eqPB = (xB - Px)^2 + (yB - Py)^2 == PB^2;
+    eqBC = (xB - OC)^2 + yB^2 == BC^2;
+    
+    % Solve the system of equations
+    solutionsA = solve([eqOA, eqPA], [xA, yA]);
+    solutionsB = solve([eqBC, eqPB], [xB, yB]);
+    
+    % Convert solutions to numeric values
+    solutions_x_a = double(solutionsA.xA);
+    solutions_y_a = double(solutionsA.yA);
+    solutions_x_b = double(solutionsB.xB);
+    solutions_y_b = double(solutionsB.yB);
+    
+    % Pick correct solutions based on motion direction
+    if solutions_x_a(1) > 0
+        x_a = solutions_x_a(2);
+        y_a = solutions_y_a(2);
+    else
+        x_a = solutions_x_a(1);
+        y_a = solutions_y_a(1);
+    end
+
+    if solutions_x_b(1) < OC
+        x_b = solutions_x_b(2);
+        y_b = solutions_y_b(2);
+    else
+        x_b = solutions_x_b(1);
+        y_b = solutions_y_b(1);
+    end
+    
+    % Define points
+    O = [0, 0];
+    A = [x_a, y_a];
+    C = [OC, 0];
+    B = [x_b, y_b];
+    P = [Px, Py];
+
+    % Clear previous plot
+    cla;
+    
+    % Plot points
+    plot([O(1), A(1)], [O(2), A(2)], 'k-', 'LineWidth', 2); % Line OA
+    plot([B(1), C(1)], [B(2), C(2)], 'k-', 'LineWidth', 2); % Line BC
+    plot([O(1), C(1)], [O(2), C(2)], 'k-', 'LineWidth', 2); % Line OC
+    plot([P(1), A(1)], [P(2), A(2)], 'k-', 'LineWidth', 2); % Line PA
+    plot([P(1), B(1)], [P(2), B(2)], 'k-', 'LineWidth', 2); % Line PB
+    
+    % Plot markers
+    plot([O(1), A(1), C(1), B(1), P(1)], [O(2), A(2), C(2), B(2), P(2)], 'bo', 'MarkerSize', 8, 'MarkerFaceColor', 'b');
+
+    % Update frame
+    pause(0.02);
+end
+
+hold off;
+
+% Function to calculate angle given three points
+function theta_deg = angle_ABC(A, B, C)
+    % Extract coordinates
+    x1 = A(1); y1 = A(2);
+    x2 = B(1); y2 = B(2);
+    x3 = C(1); y3 = C(2);
+
+    % Compute vectors AB and BC
+    AB = [x1 - x2, y1 - y2];
+    BC = [x3 - x2, y3 - y2];
+
+    % Compute dot product and magnitudes
+    dot_product = dot(AB, BC);
+    mag_AB = norm(AB);
+    mag_BC = norm(BC);
+
+    % Compute the angle in radians
+    theta_rad = acos(dot_product / (mag_AB * mag_BC));
+
+    % Convert to degrees
+    theta_deg = rad2deg(theta_rad);
+end
